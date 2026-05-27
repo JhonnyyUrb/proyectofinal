2a1bb4c3f47fc4ac756299e8846b2446b0d944d6
KennethC01 <kennethcaballerol14@gmail.com>
2026-05-27 15:17:49 -0600

    Kenneth (#5)
    * authControllers
    
    * AuthController hecho
    
    * AuthController Hecho
    
    * AuthController Hecho
    
    * guardando cambios antes de limpiar config
    
    * Eliminar configuración de Rider y limpiar entorno
    
    * Limpiar archivos .idea del historial de Git

---
+++ Controllers/AuthControllers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoClaseQ2.DTOs;
+using ProyectoClaseQ2.Services; //importar el namespace de la interfaz
+
+
+namespace ProyectoClaseQ2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService; // Cambiamos a la interfaz
+
+        
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+        {
+            try
+            {
+                var user = await _authService.Register(dto);
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+        }
+        // ... método Login similar
+    }
+}
